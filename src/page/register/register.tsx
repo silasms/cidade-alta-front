2e17947f1f2865ed8589da24918e16ac69761e48
silasms <silas.medeiros7@gmail.com>
2024-06-15 18:34:36 -0300

    feat: register integrate with back

--- src/page/register/register.tsx
+++ src/page/register/register.tsx
@@ -1,10 +1,34 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { api } from '../../service/axios';
+import { useNavigate } from 'react-router-dom';
 
 export function Register() {
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const email = (document.querySelector("input[type=email]") as HTMLInputElement).value
+    const password = (document.querySelector("input[type=password]") as HTMLInputElement).value
+    const name = (document.querySelector("input[type=text]") as HTMLInputElement).value
+
+    await register(email, password, name)
+  }
+
+  async function register(email: string, password: string, name: string) {
+    const response = await api.post('/users', {
+      email,
+      password,
+      name
+    })
+    console.log(response.data)
+    navigate('/login')
+  }
+  
+
   return (
     <main className="bg-gray-200 h-screen w-full">
       <div className="w-full h-full flex justify-center items-center">
-        <form action="" className="min-w-96 py-10 flex flex-col items-center gap-3 bg-white rounded-lg shadow-xl">
+        <form onSubmit={handleSubmit} className="min-w-96 py-10 flex flex-col items-center gap-3 bg-white rounded-lg shadow-xl">
           <div className='flex justify-start w-full' style={{marginTop: '-35px', marginLeft: '10px'}}>
             <a href="/login">
               <ArrowBackIcon style={{fill: '#00000050'}}/>
@@ -23,7 +47,7 @@ export function Register() {
             <label htmlFor="password">Senha</label>
             <input className="rounded-lg w-72 px-3 py-2 outline outline-1 outline-gray-300" type="password" name="password" id="password" placeholder="Senha" />
           </div>
-            <button className="bg-blue-400 px-7 py-1.5 rounded-lg hover:scale-105 ease-in-out duration-300">Registrar</button>
+            <button className="bg-blue-400 px-7 py-1.5 rounded-lg hover:scale-105 ease-in-out duration-300" type='submit'>Registrar</button>
         </form>
       </div>
     </main>
