f1f650375ef9f6f77ced963ef8b3cf2eadb6a8aa
silasms <silas.medeiros7@gmail.com>
2024-06-16 15:36:43 -0300

    feat: add toastify in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./routes/router";
 import { AuthProvider } from "./context/authContext";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export function App() {
   return (
     <div className="h-screen max-h-full">
       <AuthProvider>
         <BrowserRouter>
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
           <Router />
         </BrowserRouter>
       </AuthProvider>
